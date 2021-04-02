147191ce3089b89f667c15b24429a9073d42220f
Saif Ali <saif.ali@sudoconsultants.com>
2021-04-02 20:12:58 +0500

    update functionality
    Removed sns email additional functionality. Added response status to cloudformation output

--- main.tf
+++ main.tf
@@ -59,19 +59,6 @@ data "aws_iam_policy_document" "cf_backed_sns" {
   }
 }
 
-# Create a SNS topic for internal email
-resource "aws_sns_topic" "lambda_email_sns" {
-  name = "lambda_email_sns"
-}
-
-# Send Email to suscribe email to lambda_email_sns topic
-module "sns-email-subscription" {
-  source  = "QuiNovas/sns-email-subscription/aws"
-  version = "0.0.3"
-  email_address = var.email_address
-  topic_arn = aws_sns_topic.lambda_email_sns.arn
-}
-
 # Create IAM Role for Lambda Custom Function
 resource "aws_iam_role" "send_cf_updates" {
   name = "role_send_cf_updates"
@@ -106,20 +93,7 @@ resource "aws_lambda_function" "send_cf_updates" {
   role          = aws_iam_role.send_cf_updates.arn
   handler       = "lambda_function.lambda_handler"
   runtime = "python3.8"
-  # runtime = "nodejs12.x"
-
   source_code_hash = filebase64sha256(data.archive_file.init.output_path)
-
-  # Send SNS topic ARN as ENV
-  environment {
-    variables = {
-      SNS_TOPIC_ARN = aws_sns_topic.lambda_email_sns.arn
-    }
-  }
-
-  depends_on = [
-    aws_sns_topic.lambda_email_sns
-  ]
 }
 
 # Gives cf_backed_sns permission to access the send_cf_updates Lambda function.
@@ -138,51 +112,3 @@ resource "aws_sns_topic_subscription" "cf_backed_sns_to_send_cf_updates" {
   raw_message_delivery = false
   topic_arn = aws_sns_topic.cf_backed_sns.arn
 }
-
-# Permission policy for send_cf_updates to use lambda_email_sns
-data "aws_iam_policy_document" "lambda_email_sns" {
-  statement {
-    sid = ""
-    actions = [
-      "sns:ListSubscriptionsByTopic",
-      "sns:Publish",
-      "sns:Subscribe"
-    ]
-    resources = [
-      aws_sns_topic.lambda_email_sns.arn,
-    ]
-    effect = "Allow"
-  }
-}
-
-resource "aws_iam_policy" "lambda_email_sns" {
-  name   = "lambda_email_sns"
-  description = "Allow SNS limited access"
-  policy = data.aws_iam_policy_document.lambda_email_sns.json
-}
-
-resource "aws_iam_role_policy_attachment" "send_cf_updates_lambda_email_sns" {
-  role       = aws_iam_role.send_cf_updates.name
-  policy_arn = aws_iam_policy.lambda_email_sns.arn
-}
-
-
-
-# FOR TESTING: Invoke Lambda, Destination SNS
-/*
-resource "aws_lambda_function_event_invoke_config" "testing" {
-  function_name = aws_lambda_function.send_cf_updates.function_name
-  destination_config {
-    on_failure {
-      destination = aws_sns_topic.lambda_email_sns.arn
-    }
-    on_success {
-      destination = aws_sns_topic.lambda_email_sns.arn
-    }
-  }
-  depends_on = [
-    aws_sns_topic.lambda_email_sns,
-    aws_lambda_function.send_cf_updates
-  ]
-}
-*/
